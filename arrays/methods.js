51097eb8c1ced75499fd4db598703e9bb0f81b43
Akhil-Kasaraneni <akhilkasaraneni17@gmail.com>
2024-01-06 12:46:18 +0530

    array methods

---
+++ arrays/methods.js
@@ -0,0 +1,12 @@
+// reference : https://www.geeksforgeeks.org/javascript-array-reference/
+
+
+// Common methods of arrays in js
+
+
+// Array.isArray() will check whether if its an array ; same as isinstanceof opearator
+
+
+// pop will pop off the last element
+
+// lastIndexOf(element, start = 0) ; if none returns -1 
