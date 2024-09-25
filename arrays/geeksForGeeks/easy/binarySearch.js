77120f30d18f6e482b8d8e8dc9467700a8ddf5e5
Akhil-Kasaraneni <akhilkasaraneni17@gmail.com>
2024-09-25 13:05:56 +0530

    bst with tracking

--- arrays/geeksForGeeks/easy/binarySearch.js
+++ arrays/geeksForGeeks/easy/binarySearch.js
@@ -100,4 +100,29 @@ function binarySearchAlgo(arr,num){
 
 console.log(binarySearchAlgo(arr,90))
 
+
+const binarySearchTrack = (arr,num)=>{
+    let low = 0
+    let high = arr.length - 1
+    let lastPos = -1
+    while(low <=high){
+        mid = Math.floor((low+high)/2)
+        if(arr[mid] == num){
+            lastPos =  mid
+            low = mid +1
+        }
+        else if (arr[mid] < num){
+            high = mid -1
+        }
+        else{
+            low = mid + 1
+        }
+    }
+    return lastPos
+}
+console.log(binarySearch(arr,1)+1)
+
+
+
+
 // export default binarySearchAlgo
--- src/repl.js
+++ src/repl.js
@@ -1,28 +1,25 @@
-// Given an array of strings arr. Return the longest common prefix among each and every strings present in the array. If there's no prefix common in all the strings, return "-1".
+const arr = [ 1,1, 1, 1, 0, 0, 0];
 
-let arr = ["geeksforgeeks", "geeks", "geek", "geezer"]
-function longestCommonPrefix(arr) {
-    if(!arr || !arr.length) return -1
-    if(arr.length ==1) return arr[0]
-    let commonString = arr[0]
-    for(let i=1;i<arr.length;i++){
-        if(commonString.length < arr[i].length){
-            commonString = arr[i]
+
+const binarySearch = (arr,num)=>{
+    let low = 0
+    let high = arr.length - 1
+    let lastPos = -1
+    while(low <=high){
+        mid = Math.floor((low+high)/2)
+        if(arr[mid] == num){
+            lastPos =  mid
+            low = mid +1
+        }
+        else if (arr[mid] < num){
+            high = mid -1
+        }
+        else{
+            low = mid + 1
         }
     }
-
-    let common = false
-    for(let i=commonString.length;i>0;i--){
-        let stripped = commonString.slice(0,i)
-        for(let y=0;y<arr.length;y++){
-            if(!arr[y].startsWith(stripped)) {
-                common = false
-                break
-            }
-            if(y ==arr.length-1) return stripped
-        }
-    }
-    return -1
+    return lastPos
 }
 
-console.log(longestCommonPrefix(arr))
+
+console.log(binarySearch(arr,1)+1)
