edc80f216aee1af2bc2fe49302e993883401e888
Akhil Kasaraneni <akhilkasaraneni@Apples-MacBook-Air.local>
2024-10-04 01:18:33 +0530

    print distint els

--- arrays/geeksForGeeks/easy/simpleSort.js
+++ arrays/geeksForGeeks/easy/simpleSort.js
@@ -11,24 +11,33 @@ function quickSort(arr, low = 0, high = arr.length - 1) {
 }
 
 // Helper function to partition the array
-function partition(arr, low, high) {
-    let pivot = arr[high]; // Choose the last element as the pivot
-    let i = low - 1; // Index of the smaller element
+// function partition(arr, low, high) {
+//     let pivot = arr[high]; // Choose the last element as the pivot
+//     let i = low - 1; // Index of the smaller element
 
-    for (let j = low; j < high; j++) {
-        // If the current element is smaller than or equal to the pivot
-        if (arr[j] <= pivot) {
-            i++;
-            // Swap arr[i] with arr[j]
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-        }
+//     for (let j = low; j < high; j++) {
+//         // If the current element is smaller than or equal to the pivot
+//         if (arr[j] <= pivot) {
+//             i++;
+//             // Swap arr[i] with arr[j]
+//             [arr[i], arr[j]] = [arr[j], arr[i]];
+//         }
+//     }
+
+//     // Swap the pivot element with the element at i + 1
+//     [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+//     return i + 1;
+// }
+
+// // Example usage
+// let arr = [10, 80, 30, 90, 40, 50, 70];
+// console.log(quickSort(arr)); // Output: [10, 30, 40, 50, 70, 80, 90]
+
+
+const searchSort = (arr)=>{
+    if(arr.length <=1) return true
+    for(let i=1;i<arr.length;i++){
+        if(arr[i] < arr[i-1]) return false
     }
-
-    // Swap the pivot element with the element at i + 1
-    [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
-    return i + 1;
+    return true
 }
-
-// Example usage
-let arr = [10, 80, 30, 90, 40, 50, 70];
-console.log(quickSort(arr)); // Output: [10, 30, 40, 50, 70, 80, 90]
